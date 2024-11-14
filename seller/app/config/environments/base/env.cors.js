bdf38a43c655d0ac1cfaeb588c1c7fb5b437f36e
Sudhir Verma <sudhirvvermaa@gmail.com>
2024-11-14 11:12:57 +0530

    Update env.cors.js

--- seller/app/config/environments/base/env.cors.js
+++ seller/app/config/environments/base/env.cors.js
@@ -4,7 +4,7 @@ module.exports = {
             '*',
             'http://localhost:3000',
             'https://localhost:3000',
-            'http://ec2-3-90-151-99.compute-1.amazonaws.com:3000'
+            'http://ec2-3-90-151-99.compute-1.amazonaws.com:3000',
             'http://43.205.242.64:8081'
         ]
     }
