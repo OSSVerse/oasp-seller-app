468d8d6f0c155cf693c274e41e706050f94e79af
Sudhir Verma <sudhirvvermaa@gmail.com>
2024-11-27 11:30:09 +0530

    Added o31e in whitelistUrls (#18)

--- seller/app/config/environments/base/env.cors.js
+++ seller/app/config/environments/base/env.cors.js
@@ -5,7 +5,8 @@ module.exports = {
             'http://localhost:3000',
             'https://localhost:3000',
             'http://ec2-3-90-151-99.compute-1.amazonaws.com:3000',
-            'http://oasp-sellerapp.ossverse.com'
+            'http://oasp-sellerapp.ossverse.com',
+            'http://osslab.o31e.com'
         ]
     }
 };
